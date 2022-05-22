87cddfea3f9957dc0469ce5c951e7c576d12cf8d
koushik-sherugar <kotegarkoushik@gmail.com>
2022-05-22 16:49:09 +0530

    feat: home page created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,48 @@
 import React from 'react'
-
+// import Link from "react-router-dom"
 const Home = () => {
   return (
+    <>
 
-    <div className="h-screen flex
-    items-center justify-center">
-      <div className="">
-        <h1 className=" text-7xl ">NSS MANAGEMENT SYSTEM</h1>
-        <p className="text-xl text-center	"> a one stop store for all works</p>
 
-        <div className="btn mt-7 text-center	">
-          <button className=''>About</button>
-          <button>Get started</button>
+      <div className='bg-white h-screen flex flex-col justify-center items-center'>
+        <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>NSS MANAGENT SYSTEM</h1>
+        <div className='py-6 px-10  bg-indigo-600 rounded-full text-3xl hover:bg-indigo-300 transition duration-300 ease-in-out flex items-center animate-bounce' to='/menu'>
+          Get Started{' '}
+
+          {/* <svg xmlns="http://www.w3.org/2000/svg" class=" h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg> */}
         </div>
       </div>
-    </div>
+    </>
+    // <div className="h-screen flex
+    // items-center justify-center">
+    //   <div className="">
+    //     <h1 className=" text-7xl ">NSS MANAGEMENT SYSTEM</h1>
+    //     <p className="text-xl text-center	"> a one stop store for all works</p>
+
+    //     <div className="btn mt-7 text-center	">
+    //       <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+    //         <button
+    //           type="submit"
+    //           className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    //         >
+    //           Save
+    //         </button>
+    //       </div>
+    //       <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+    //         <button
+    //           type="submit"
+    //           className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    //         >
+    //           Save
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,37 +8,103 @@ export default function Login() {
           <div className="md:grid md:grid-cols-3 md:gap-6">
             <div className="md:col-span-1">
               <div className="px-4 sm:px-0">
-                <h3 className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+                <h3 className="text-lg font-medium leading-6 capitalize text-gray-900">Profile</h3>
                 <p className="mt-1 text-sm text-gray-600">
                   This information will be displayed publicly so be careful what you share.
                 </p>
               </div>
             </div>
+
             <div className="mt-5 md:mt-0 md:col-span-2">
               <form action="#" method="POST">
                 <div className="shadow sm:rounded-md sm:overflow-hidden">
                   <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                    <div className="grid grid-cols-3 gap-6">
-                      <div className="col-span-3 sm:col-span-2">
-                        <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
-                          Website
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="col-span-6 sm:col-span-3">
+                        <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                          First name
                         </label>
-                        <div className="mt-1 flex rounded-md shadow-sm">
-                          <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                            http://
-                          </span>
-                          <input
-                            type="text"
-                            name="company-website"
-                            id="company-website"
-                            className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                            placeholder="www.example.com"
-                          />
-                        </div>
+                        <input
+                          type="text"
+                          name="first-name"
+                          id="first-name"
+                          autoComplete="given-name"
+                          className="mt-1 focus:ring-indigo-500/50 focus:border-indigo-500/50 block w-full shadow-sm sm:text-md border border-gray-300 rounded-md"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3">
+                        <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                          Last Name
+                        </label>
+                        <input
+                          type="text"
+                          name="last-name"
+                          id="last-name"
+                          autoComplete="family-name"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-md border border-gray-300 rounded-md"
+                        />
                       </div>
                     </div>
 
-                    <div>
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="col-span-6">
+                        <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+                          Street address
+                        </label>
+                        <input
+                          type="text"
+                          name="street-address"
+                          id="street-address"
+                          autoComplete="street-address"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                          City
+                        </label>
+                        <input
+                          type="text"
+                          name="city"
+                          id="city"
+                          autoComplete="address-level2"
+                          className="mt-1 pl-2
+                           focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label htmlFor="region" className="block text-sm font-medium text-gray-700">
+                          State / Province
+                        </label>
+                        <input
+                          type="text"
+                          name="region"
+                          id="region"
+                          autoComplete="address-level1"
+                          className="mt-1 pl-2
+                           focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">
+                          ZIP / Postal code
+                        </label>
+                        <input
+                          type="text"
+                          name="postal-code"
+                          id="postal-code"
+                          autoComplete="postal-code"
+                          className="mt-1 pl-2
+                          focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
+                        />
+                      </div>
+
+                    </div>
+                    {/* <div>
                       <label htmlFor="about" className="block text-sm font-medium text-gray-700">
                         About
                       </label>
@@ -55,8 +121,27 @@ export default function Login() {
                       <p className="mt-2 text-sm text-gray-500">
                         Brief description for your profile. URLs are hyperlinked.
                       </p>
-                    </div>
+                    </div> */}
 
+                    {/* <div className="grid grid-cols-3 gap-6">
+                            <div className="col-span-3 sm:col-span-2">
+                              <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
+                                Website
+                              </label>
+                              <div className="mt-1 flex rounded-md shadow-sm">
+                                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                                  http://
+                                </span> 
+                                <input
+                                  type="text"
+                                  name="company-website"
+                                  id="company-website"
+                                  className="h-8 focus:ring-indigo-200 focus:border-indigo-300  flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+                                  placeholder="www.example.com"
+                                />
+                              </div>
+                            </div>
+                          </div> */}
                     <div>
                       <label className="block text-sm font-medium text-gray-700">Photo</label>
                       <div className="mt-1 flex items-center">
@@ -149,7 +234,7 @@ export default function Login() {
                           name="first-name"
                           id="first-name"
                           autoComplete="given-name"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-md border-gray-300 rounded-md"
                         />
                       </div>
 
@@ -246,6 +331,7 @@ export default function Login() {
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
+
                     </div>
                   </div>
                   <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
