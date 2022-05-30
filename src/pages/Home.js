74efaf7cdf805572ce57926d674ca0beb7544c70
koushik-sherugar <kotegarkoushik@gmail.com>
2022-05-30 07:37:38 +0530

    feat: routing of pages done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,13 @@ const Home = () => {
 
       <div className='bg-white h-screen flex flex-col justify-center items-center'>
         <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>NSS MANAGENT SYSTEM</h1>
-        <div className='py-6 px-10  bg-indigo-600 rounded-full text-3xl hover:bg-indigo-300 transition duration-300 ease-in-out flex items-center animate-bounce' to='/menu'>
-          Get Started{' '}
-
-          {/* <svg xmlns="http://www.w3.org/2000/svg" class=" h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg> */}
+        <div className="flex flex-row space-x-4 ">
+          <div className='py-6 px-10  bg-indigo-600 rounded-full text-3xl hover:bg-indigo-300 transition duration-300 ease-in-out flex items-center animate-bounce' to='/menu'>
+            Get Started{' '}
+          </div>
+          {/* <div className='py-6 px-10 bg-gray-300 rounded-full text-3xl hover:bg-indigo-300 transition duration-300 ease-in-out flex items-center animate-bounce' to='/menu'>
+            About
+          </div> */}
         </div>
       </div>
     </>
---
+++ src/routes/routedata.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+// import { Route, Routes } from "react-router-dom"
+
+// import routeConfig from './route-config'
+
+// function routedata() {
+//     const { routes } = routeConfig
+
+//     return (
+
+//         <Routes>
+//             {routes.map((routes, i) => {
+//                 const { component: Component, path, exact } = route;
+//                 return (
+//                     <Route path={path} component={Component} exact={exact} keys={i} />
+//                 )
+//             })}
+//         </Routes>
+
+//     )
+// }
+
+// export default routedata
